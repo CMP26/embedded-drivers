5fce1a994ad1a7efaabc7ddd604884f1291114e1
Ahmed Kamal <98265644+ahmed-kamal2004@users.noreply.github.com>
2025-05-03 03:04:50 +0300

    Create parking_detector.h

---
+++ parking_detector.h
@@ -0,0 +1,19 @@
+#ifndef PARKING_DETECTOR_H
+#define PARKING_DETECTOR_H
+
+// Thresholds and constants
+#define OBSTACLE_THRESHOLD_CM 25.0
+#define MIN_PARK_WIDTH_CM    20.0
+
+typedef enum {
+    SEARCHING_FIRST_OBSTACLE,
+    FOUND_FIRST_OBSTACLE,
+    FOUND_EMPTY_SPACE,
+    FOUND_SECOND_OBSTACLE
+} DetectionState;
+
+// Function declarations
+DetectionState updateParkingDetection(float obstacleDistance, unsigned long currentTime, float carSpeed);
+int isParkingSpotFound(void);
+
+#endif
